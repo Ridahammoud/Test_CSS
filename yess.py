9e681a5741b671f0ea337d1b8a7e49171311b2a1
Ridahammoud <rida.hammoud@aesma.eu>
2024-12-04 16:38:44 +0100

    yess.py

--- yess.py
+++ yess.py
@@ -21,7 +21,7 @@ def convert_df_to_xlsx(df):
     
 def format_ligne(ligne):
     return f"""
-    **Date**: {col_date}
+    **Date**: {date_debut}
     **Opérateur**: {ligne['Prénom et nom']}
     **Équipement**: {ligne['Équipement']}
     **Localisation**: {ligne['Localisation']}
